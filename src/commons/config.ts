1a79f5e8840b394cf0693697b8b15466aaf287b3
adamjpatterson <adam@farar.net>
2024-08-05 22:08:14 -0400

    Remove EventEmitter super, accessors/mutators.

--- src/commons/config.ts
+++ src/commons/config.ts
@@ -1,21 +1,19 @@
 import * as stream from 'node:stream';
-import { EventEmitter } from 'node:events';
 
 export type ErrorHandler = (err: Error, ...params: Array<unknown>) => void;
 
-class Config extends EventEmitter {
+class Config {
 
     protected _highWaterMark?: number;
     protected _highWaterMarkObjectMode?: number;
-    protected _captureStackTrace: boolean;
-    protected _captureISOTime: boolean;
-    protected _errorHandler: ErrorHandler;
+    public captureStackTrace: boolean;
+    public captureISOTime: boolean;
+    public errorHandler: ErrorHandler;
 
     constructor() {
-        super();
-        this._captureStackTrace = true;
-        this._captureISOTime = true;
-        this._errorHandler = console.error;
+        this.captureStackTrace = true;
+        this.captureISOTime = true;
+        this.errorHandler = console.error;
     }
 
     public get highWaterMark(): number {
@@ -32,33 +30,6 @@ class Config extends EventEmitter {
 
     public set highWaterMarkObjectMode(highWaterMarkObjectMode: number) {
         this._highWaterMarkObjectMode = highWaterMarkObjectMode;
-    }
-
-    public get captureStackTrace(): boolean{
-        return this._captureStackTrace;
-    }
-
-    public set captureStackTrace(captureStackTrace: boolean) {
-        this._captureStackTrace = captureStackTrace;
-        this.emit('captureStackTrace', captureStackTrace);
-    }
-
-    public get captureISOTime(): boolean{
-        return this._captureISOTime;
-    }
-
-    public set captureISOTime(captureISOTime: boolean) {
-        this._captureISOTime = captureISOTime;
-        this.emit('captureISOTime', captureISOTime);
-    }
-
-    public get errorHandler() {
-        return this._errorHandler;
-    }
-
-    public set errorHandler(errorHandler: ErrorHandler) {
-        this._errorHandler = errorHandler;
-        this.emit('errorHandler', errorHandler);
     }
 
     public getWritableOptions(objectMode: boolean = true): stream.WritableOptions {
