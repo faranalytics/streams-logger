3e6ec481fff5b747f4237f27ed84f5f390c9372e
Adam Patterson <adam@farar.net>
2025-05-11 15:05:55 -0400

    The formatter mutates the LogContext; hence, make a copy.

--- src/formatters/formatter.ts
+++ src/formatters/formatter.ts
@@ -19,8 +19,10 @@ export class Formatter<MessageInT = string, MessageOutT = string> extends Node<L
         transform: (logContext: LogContext<MessageInT, SyslogLevelT>, encoding: BufferEncoding, callback: stream.TransformCallback) => {
           void (async () => {
             try {
-              ((logContext as unknown) as LogContext<MessageOutT, SyslogLevelT>).message = await format(logContext);
-              callback(null, logContext);
+              const message = await format(logContext);
+              // eslint-disable-next-line @typescript-eslint/no-misused-spread
+              const logContextOut = new LogContext<MessageOutT, SyslogLevelT>({ ...logContext, ...{ message: message } });
+              callback(null, logContextOut);
             }
             catch (err) {
               if (err instanceof Error) {
