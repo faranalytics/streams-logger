eb16c835d10679bd902defae58c300e4322a3643
Adam Patterson <adam@farar.net>
2025-05-10 20:17:02 -0400

    Improve tests.

--- tests/console_handler/src/index.ts
+++ tests/console_handler/src/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import * as fs from 'node:fs';
-import { Logger, Formatter, ConsoleHandler, SyslogLevel } from 'streams-logger';
+import * as fs from "node:fs";
+import { Logger, Formatter, ConsoleHandler, SyslogLevel } from "streams-logger";
 
 // fs.readdirSync('.', { withFileTypes: true }).forEach((value: fs.Dirent) => {
 //     if (value.name.match(/[^.]+.log.\d+/)) {
@@ -10,23 +10,23 @@ import { Logger, Formatter, ConsoleHandler, SyslogLevel } from 'streams-logger';
 
 const logger = new Logger({ level: SyslogLevel.DEBUG });
 const consoleFormatter = new Formatter({
-    format: ({level, isotime, hostname, pid, func, message,  }) => (
-        `<${level}> ${isotime} ${hostname} ${pid} - ${func} ${message}\n`
-    )
+  format: ({ level, isotime, hostname, pid, func, message, }) => (
+    `<${level}> ${isotime ?? ""} ${hostname ?? ""} ${pid?.toString() ?? ""} - ${func ?? ""} ${message}\n`
+  )
 });
 
 const consoleHandler = new ConsoleHandler({ level: SyslogLevel.DEBUG });
 
 const log = logger.connect(
-    consoleFormatter.connect(
-        consoleHandler
-    )
+  consoleFormatter.connect(
+    consoleHandler
+  )
 );
 
 function sayHello() {
-    for (let i = 0; i < 1e1; i++) {
-        log.debug('Hello, World!');
-    }
+  for (let i = 0; i < 1e1; i++) {
+    log.debug("Hello, World!");
+  }
 }
 
 setInterval(sayHello, 1000);
