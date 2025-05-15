4ab30de21dd1e1e999731eb76ebbb04ef7de048a
Adam Patterson <adam@farar.net>
2025-05-15 18:56:08 -0400

    Improve tests.

--- tests/spread_logger/src/index.ts
+++ tests/spread_logger/src/index.ts
@@ -1,30 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Formatter, ConsoleHandler, SyslogLevel } from "streams-logger";
 import { SpreadLogger } from "./spread_logger.js";
 
-const logger = new SpreadLogger<[string, object]>({ level: SyslogLevel.DEBUG });
-const consoleFormatter = new Formatter<[string, object], string>({
-  format: ({ level, isotime, hostname, pid, func, message, }) => (
-    `<${level}> ${isotime ?? ""} ${hostname ?? ""} ${pid?.toString() ?? ""} - ${func ?? ""} ${message[0]}:${JSON.stringify(message[1])}\n`
-  )
-});
-const consoleHandler = new ConsoleHandler({ level: SyslogLevel.DEBUG });
+for (let i = 0; i < 20; i++) {
+  const logger = new SpreadLogger<[string, object]>({ level: SyslogLevel.DEBUG });
+  const consoleFormatter = new Formatter<[string, object], string>({
+    format: ({ level, isotime, hostname, pid, func, message, }) => (
+      `<${level}> ${isotime ?? ""} ${hostname ?? ""} ${pid?.toString() ?? ""} - ${func ?? ""} ${message[0]}:${JSON.stringify(message[1])}\n`
+    )
+  });
+  const consoleHandler = new ConsoleHandler({ level: SyslogLevel.DEBUG });
 
-const log = logger.connect(
-  consoleFormatter.connect(
-    consoleHandler
-  )
-);
+  const log = logger.connect(
+    consoleFormatter.connect(
+      consoleHandler
+    )
+  );
 
-function sayHello() {
-  for (let i = 0; i < 1e0; i++) {
-    log.debug("Label", { greeting: "Hello, World!" });
+  function sayHello() {
+    for (let i = 0; i < 1e0; i++) {
+      log.debug("Label", { greeting: "Hello, World!" });
+    }
   }
-}
 
-setInterval(sayHello, 1000);
+  setInterval(sayHello, 1000);
 
-log.debug("Label", { greeting: "Hello, World!" });
-
-(function sayHello() {
   log.debug("Label", { greeting: "Hello, World!" });
-})();
+
+  (function sayHello() {
+    log.debug("Label", { greeting: "Hello, World!" });
+  })();
+}
